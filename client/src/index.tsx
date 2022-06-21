7599022fa04a3caf3d3ea90331dc0045dd8e37fa
limingcong <limingcong@shimo.im>
2022-06-21 15:08:05 +0800

    chore: add prettier

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const App = () => {
-  console.log("render");
   return <div>hello world</div>;
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
